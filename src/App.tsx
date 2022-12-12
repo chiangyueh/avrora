c5b6e8b834ec209883ebede39ecedb99912efcae
yueh <y456123u@gmail.com>
2022-12-13 01:14:22 +0800

    feat: 新增menu組件

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import Button,{ButtonType,ButtonSize} from './components/Button/button'
-import Alert,{AlertType} from './components/Alert/alert';
+import Menu from './components/Menu/menu';
+import MenuItem from './components/Menu/menuItem';
+import SubMenu from './components/Menu/subMenu';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu defaultIndex='0' mode='vertical' onSelect={(index)=>{console.log(index)}} defaultOpenSubMenus={['2']}>
+          <MenuItem>
+            cool1
+          </MenuItem>
+          <MenuItem>
+            cool2
+          </MenuItem>
+          <SubMenu title='dropdown'>
+            <MenuItem>
+              dropdown1
+            </MenuItem>
+            <MenuItem>
+              dropdown2
+            </MenuItem>
+          </SubMenu>
+          <MenuItem>
+            cool3
+          </MenuItem>
+        </Menu>
 
-      <Alert title={'alert'} description={'description'} closeable></Alert>
-      <Button onClick={()=>{
-        console.log(123)
-      }}>Hellow</Button>
-      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hellow</Button>
-      <Button btnType={ButtonType.Link} href='http://www.baidu.com'>Baidu Link</Button>
+
       </header>
     </div>
   );
